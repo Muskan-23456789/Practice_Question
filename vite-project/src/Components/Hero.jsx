72a59433a5c452efc651d84613f8b7d483342c73
Muskan-23456789 <muskanmashrak654@gmail.com>
2025-04-03 14:55:20 +0530

    done1

--- vite-project/src/Components/Hero.jsx
+++ vite-project/src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <div className="bg-gray-600 text-white py-16">
+        <div className="bg-gray-600  text-white py-16">
             <div className="container mx-auto text-center">
                 <h1 className="text-4xl font-bold mb-4">Welcome to App</h1>
                 <p className="text-xl">Create, and organize your notes.</p>
